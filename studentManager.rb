0eb954027beb8f2cc7f62382e059cb6a6dcc0e89
patelr124 <70537858+patelr124@users.noreply.github.com>
2020-09-14 01:05:23 -0400

    added student editing and file name input

--- studentManager.rb
+++ studentManager.rb
@@ -11,7 +11,7 @@ require 'pp'
 #   methods related to csv file loading and exporting
 #########################################################################
 def load_students(filename)
-	CSV.foreach(filename, headers: true) do |line|
+	CSV.foreach(filename.strip, headers: true) do |line|
 		first, last, email, section, major1, major2, minor1, minor2 = line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7]
 		push_student(first, last, email, section, major1, major2, minor1, minor2)
 	end
@@ -29,6 +29,40 @@ def push_student(first, last, email, section, major1, major2, minor1, minor2)
 #   methods related creating groups
 #########################################################################
 
+def create_group
+	puts "1: Section Based (students from the same section)"
+	puts "2: Major Based"
+	puts "3: Minor Based"
+	puts "4: More than 1 Basis (ex. major and section)"
+	puts "0: Back to Main Menu"
+	puts "Please type the option number that you would like:"
+	input = gets.strip
+	if input == "1"
+		section_based
+	elsif input == "2"
+		
+	elsif input == "3"
+		
+	elsif input == "4"
+		
+	elsif input == "0\n"
+		main_menu
+	else
+		puts "Invalid input! Please restart!"
+		edit_student
+	end
+
+end
+
+
+def section_based
+	# section_values = @students.uniq.pluck(:section)
+	puts "Please enter the section that you would like to split up into groups: "
+	section = gets.strip.to_s
+	section_all = section_choice(@students, section)
+	puts "How many people would you like in each group? Current population of section " +section+" is "+section_all.length
+end
+
 def section_choice(students, section) # students is the array
 	@in_section=[];
 	students.each do |student| # only select students by month
@@ -37,7 +71,7 @@ def section_choice(students, section) # students is the array
 		end
 	end
 	# print_students(@in_section)
-	@groups << @in_section
+	return @in_section
 end
 
 def major1_choice(students, major1) # students is the array
@@ -78,36 +112,134 @@ end
 #   methods related to editing a students data
 #########################################################################
 def edit_student
-	puts "What is the first name of the student whose information you would like to change: "
-	firstname = gets
-	firstname.chomp
-	puts "What is the last name of the student whose information you would like to change: "
-	lastname = gets
-	lastname.chomp
+	puts "What is the email of the student whose information you would like to change: "
+	email = gets
+	email.chomp
+	# puts "What is the last name of the student whose information you would like to change: "
+	# lastname = gets
+	# lastname.chomp
 	@students.each do |student|
-		puts student[:first]
-		if student[:first].to_s == firstname #&& "#{student[:first]}" == lastname.t
+		# puts "#{student[:email]}".strip.eql? email.strip
+		if "#{student[:email]}".strip.eql? email.strip
 			puts "Student found. What would you like to change about the student?"
 			puts "1: First Name"
 			puts "2: Last Name"
 			puts "3: Email"
-			puts "4: Major (1st or 2nd)"
-			puts "5: Minor (1st or 2nd"
+			puts "4: Section"
+			puts "5: Major (1st)        6: Major (2nd)"
+			puts "7: Minor (1st)        8: Minor (2nd)"
+			puts "0: Back to Main Menu"
 			puts "Please type the option number that you would like:"
 			change = gets
 			if change == "1\n"
 				puts "What would you like their first name to be?"
 				change = gets
 				change.chomp
-				instance_variable_set(:@first, change)
-				puts "#{student[:first]} #{student[:last]}"
+				student[:first] = change.strip
+			elsif change == "2\n"
+				puts "What would you like their last name to be?"
+				change = gets
+				change.chomp
+				student[:last] = change.strip
+			elsif change == "3\n"
+				puts "What would you like their email to be?"
+				change = gets
+				change.chomp
+				student[:email] = change.strip
+			elsif change == "4\n"
+				puts "What would you like their section to be?"
+				change = gets
+				change.chomp
+				student[:section] = change.strip
+			elsif change == "5\n"
+				puts "What would you like their major (1st) to be?"
+				change = gets
+				change.chomp
+				student[:major1] = change.strip
+			elsif change == "6\n"
+				puts "What would you like their major (2st) to be?"
+				change = gets
+				change.chomp
+				student[:major2] = change.strip
+			elsif change == "7\n"
+				puts "What would you like their minor (1st) to be?"
+				change = gets
+				change.chomp
+				student[:minor1] = change.strip
+			elsif change == "8\n"
+				puts "What would you like their minor (2st) to be?"
+				change = gets
+				change.chomp
+				student[:minor2] = change.strip
+			elsif input == "0\n"
+				main_menu
+			else
+				puts "Invalid input! Please restart!"
+				edit_student
 			end
 		end
 	end
 end
 
+def student_options
+	puts "What would you like to do?"
+	puts "1: Edit existing student"
+	puts "2: Create new student"
+	puts "3: Delete existing student"
+	puts "0: Back to Main Menu"
+	input = gets
+	if input == "1\n"
+		edit_student
+	elsif input =="2\n"
+		create_student
+	elsif input == "3\n"
+		delete_student
+	elsif input == "0\n"
+		main_menu
+	else 
+		puts "Invalid student search, please restart!"
+		student_options
+	end	
+end
+
+def create_student
+	puts "What is the student's first name?"
+	first = gets.strip
+	puts "What is the student's last name?"
+	last = gets.strip
+	puts "What is the student's email?"
+	email = gets.strip
+	puts "What's the student's section number?"
+	section = gets.strip
+	puts "What's the students major (1st)?"
+	major1 = gets.strip
+	puts "What's the student's major (2nd)?"
+	major2 = gets.strip
+	puts "What's the students minor (1st)?"
+	minor1 = gets.strip
+	puts "What's the student's minor (2nd)?"
+	minor2 = gets.strip
+	push_student(first, last, email, section, major1, major2, minor1, minor2)
+end
 
 
+def delete_student
+	puts "What is the email of the student whose information you would like to change: "
+	email = gets
+	email.chomp
+	index_del = nil;
+	@students.each_with_index do |student, index|
+		if "#{student[:email]}".strip.eql? email.strip
+			index_del = index
+		end
+	end
+	if index_del == nil
+		puts "Invalid student search, please restart!"
+		student_options
+	else @students.delete_at(index_del)
+	end
+
+end
 #########################################################################
 #   methods related to the main menu and choice evaluation
 #########################################################################
@@ -118,15 +250,14 @@ def menu_choice(choice)
 		puts "Please enter filename: "
 		input = gets
 		input.chomp
-		load_students("test_file.csv")
+		load_students(input.strip)
 		main_menu
 	elsif choice == "1\n"
-		edit_student
+		student_options
 		main_menu
-
-	# elsif choice == "2\n"
-	# 	create_group
-	# 	main_menu
+	elsif choice == "2\n"
+		create_group
+		main_menu
 	# elsif choice == "3\n"
 	# 	export_groups
 	# 	main_menu
@@ -142,7 +273,7 @@ end
 def main_menu
 	puts "----- Main Menu ------"
 	puts "0: Load .csv File"
-	puts "1: Edit Student"
+	puts "1: Edit/Add/Delete Student"
 	puts "2: Create Group"
 	puts "3: Write All Groups to File"
 	puts "4: View All Students"
@@ -157,6 +288,7 @@ end
 #########################################################################
 
 # load_students("test_file.csv")
+# edit_student
 # print_students (@students)
 # section_choice(@students, 9)
 # section_choice(@students, 3)
